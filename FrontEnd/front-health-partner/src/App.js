23f5965d39bd154f92006a909a21ed292c96723b
Helon de França <92336676+helonfranca@users.noreply.github.com>
2024-08-21 20:54:17 -0300

    refactor: add component about

--- FrontEnd/front-health-partner/src/App.js
+++ FrontEnd/front-health-partner/src/App.js
@@ -1,11 +1,14 @@
 // src/App.js
-import React  from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import WelcomeSection from './components/WelcomeSection/WelcomeSection';
 import About from './components/About/About';
 import Profile from './components/Profile/Profile';
+import SmartUserProfData from './components/SmartUserProfData/SmartUserProfData.js';
+import SmartUserData from './components/SmartUserData/SmartUserData.js';
+import DadosSmart from './components/DadosSmart/DadosSmart.js';
 
 function App() {
   return (
@@ -16,7 +19,12 @@ function App() {
           <Route path="/" element={<WelcomeSection />} />
           <Route path="/Sobre" element={<About />} />
           <Route path="/Perfil" element={<Profile />} />
-          {/* Outras rotas podem ser adicionadas aqui */}
+          <Route path="/Relatorio" element={<SmartUserData />} />
+          <Route path="/DadosSmart" element={<DadosSmart />} />
+          <Route
+            path="/RelatorioProfissional"
+            element={<SmartUserProfData />}
+          />
         </Routes>
       </div>
     </Router>
