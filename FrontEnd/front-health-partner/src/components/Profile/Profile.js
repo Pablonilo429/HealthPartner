b950da439d09af1fb0a893d1fbeabe344b82d874
Helon de França <92336676+helonfranca@users.noreply.github.com>
2024-08-21 20:55:35 -0300

    refactor: add component profile

--- FrontEnd/front-health-partner/src/components/Profile/Profile.js
+++ FrontEnd/front-health-partner/src/components/Profile/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Profile.css'; // Importe o arquivo CSS para estilização
+import { NavLink } from 'react-router-dom';
 
 function Profile() {
   return (
@@ -16,12 +17,18 @@ function Profile() {
               alt="Uma idosa e uma jovem sentadas no parque sorrindo"
             />
             <div className="card-body text-center">
-              <p className="card-text fw-bold">Ver os dados do meu smartwatch de maneira fácil e descomplicada!</p>
+              <p className="card-text fw-bold">
+                Ver os dados do meu smartwatch de maneira fácil e descomplicada!
+              </p>
               <div className="text-center mb-1 p-2 mb-lg-4">
-                   {/* Mudar para usar o Link ou router */}
-                <a href="DadosSmart.html" className="btn btn-dark fw-bold fs-6">
-                  Clique aqui para selecionar esta opção
-                </a>
+                <p className="btn btn-dark fw-bold fs-6">
+                  <NavLink
+                    to="/Relatorio"
+                    className="nav-link active  text-light fw-bold"
+                  >
+                    Clique aqui para selecionar esta opção
+                  </NavLink>
+                </p>
               </div>
             </div>
           </div>
@@ -34,12 +41,19 @@ function Profile() {
               alt="Estetoscopio e teclado"
             />
             <div className="card-body text-center">
-              <p className="card-text fw-bold">Sou profissional da saúde e busco informações sobre o meu paciente</p>
+              <p className="card-text fw-bold">
+                Sou profissional da saúde e busco informações sobre o meu
+                paciente
+              </p>
               <div className="text-center mb-1 p-2 mb-lg-4">
-                {/* Mudar para usar o Link ou router */}
-                <a href="Relatorioprofissional.html" className="btn btn-dark fw-bold fs-6">
-                  Clique aqui para selecionar esta opção
-                </a>
+                <p className="btn btn-dark fw-bold fs-6">
+                  <NavLink
+                    to="/RelatorioProfissional"
+                    className="nav-link active  text-light fw-bold"
+                  >
+                    Clique aqui para selecionar esta opção
+                  </NavLink>
+                </p>
               </div>
             </div>
           </div>
