2a47de6d8513a193d1e54762adf91d3a4dc0c9d6
Helon de França <92336676+helonfranca@users.noreply.github.com>
2024-08-21 21:02:10 -0300

    refactor: add component WelcomeSection

--- FrontEnd/front-health-partner/src/components/WelcomeSection/WelcomeSection.js
+++ FrontEnd/front-health-partner/src/components/WelcomeSection/WelcomeSection.js
@@ -4,22 +4,32 @@ import './WelcomeSection.css';
 
 function WelcomeSection() {
   return (
-    <div className="container pt-lg-2 mt-5 text-light welcome-section">
+    <div
+      id="profile-container"
+      className="container pt-lg-2 mt-5 text-light welcome-section"
+    >
       <div className="mt-3 text-center">
         <h1>Bem-vindo ao Saúde Mais</h1>
       </div>
       <div className="pt-lg-5 p-4 text-center">
-        <h4>Aqui você terá acesso aos dados monitorados pelo seu SmartWatch e a relatórios sobre sua saúde!</h4>
+        <h4>
+          Aqui você terá acesso aos dados monitorados pelo seu SmartWatch e a
+          relatórios sobre sua saúde!
+        </h4>
       </div>
       <div className="pt-lg-2 p-2 m-2 text-center">
         <div className="m-lg-2">
           <button type="button" className="btn btn-light fs-6 fw-bold mb-2">
-            <Link to="/Perfil" className="nav-link active text-dark fw-bold">Clique aqui para começar</Link>
+            <Link to="/Perfil" className="nav-link active text-dark fw-bold">
+              Clique aqui para começar
+            </Link>
           </button>
         </div>
         <div className="m-lg-2">
           <button type="button" className="btn btn-light fs-6 fw-bold mb-2">
-            <Link to="/Sobre" className="nav-link active text-dark fw-bold">Clique aqui para saber mais</Link>
+            <Link to="/Sobre" className="nav-link active text-dark fw-bold">
+              Clique aqui para saber mais
+            </Link>
           </button>
         </div>
       </div>
@@ -28,5 +38,3 @@ function WelcomeSection() {
 }
 
 export default WelcomeSection;
-
-
